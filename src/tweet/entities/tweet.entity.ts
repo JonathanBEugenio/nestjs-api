e7ac52351d41018c71e17c4e33e4e114d006794e
Jonathan Brendon Eugênio <jonathan.eugenio@leucotron.com.br>
2022-10-23 09:44:33 -0300

    add tweet schema

--- src/tweet/entities/tweet.entity.ts
+++ src/tweet/entities/tweet.entity.ts
@@ -1,1 +1,21 @@
-export class Tweet {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export type TweetProps = {
+  content: string;
+  displayName: string;
+}
+
+@Schema()
+export class Tweet {
+  constructor(props: TweetProps) {
+    Object.assign(this, props);
+  }
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ required: true })
+  displayName: string;
+}
+
+export const TweetSchema = SchemaFactory.createForClass(Tweet);
